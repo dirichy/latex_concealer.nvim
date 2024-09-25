2b96828f207e57dc11dd932765d30ec8ed5daca8
dirichy <144102919+dirichy@users.noreply.github.com>
2024-09-26 03:52:59 +0800

    update

--- lua/latex_concealer/init.lua
+++ lua/latex_concealer/init.lua
@@ -157,6 +157,7 @@ function M.refresh(buffer)
 		0,
 		vim.schedule_wrap(function()
 			-- vim.api.nvim_buf_clear_namespace(0, vim.api.nvim_create_namespace("latex_concealer"), 0, -1)
+			counter.reset_all(buffer)
 			M.conceal(buffer)
 		end)
 	)
