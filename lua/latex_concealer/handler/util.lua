2bc3a8eabe6ac4ff6729dbfddbbd16ad8f0b6ffa
dirichy <144102919+dirichy@users.noreply.github.com>
2024-09-27 17:43:39 +0800

    update

--- lua/latex_concealer/handler/util.lua
+++ lua/latex_concealer/handler/util.lua
@@ -19,25 +19,17 @@ function M.conceal_commands(opts)
 				col1,
 				row1_end,
 				col1_end + 1,
-				opts.delim[index],
+				opts.delim[index] or "",
 				vim.api.nvim_create_namespace("latex_concealer")
 			)
 			if opts.map[index] then
 				local text = vim.treesitter.get_node_text(arg_node, buffer):sub(2, -2)
-				util.multichar_conceal(
-					buffer,
-					row1_end,
-					col1_end + 1,
-					row2,
-					col2 - 1,
-					{
-						string.gsub(text, ".", function(str)
-							return opts.map[index][1][str] or str
-						end),
-						opts.map[index][2],
-					},
-					vim.api.nvim_create_namespace("latex_concealer")
-				)
+				util.multichar_conceal(buffer, row1_end, col1_end + 1, row2, col2 - 1, {
+					string.gsub(text, ".", function(str)
+						return opts.map[index][1][str] or str
+					end),
+					opts.map[index][2],
+				}, vim.api.nvim_create_namespace("latex_concealer"))
 			end
 			row1 = row2
 			col1 = col2 - 1
@@ -49,7 +41,7 @@ function M.conceal_commands(opts)
 				col1,
 				row1,
 				col1 + 1,
-				opts.delim[#opts.delim],
+				opts.delim[#opts.delim] or "",
 				vim.api.nvim_create_namespace("latex_concealer")
 			)
 		end
