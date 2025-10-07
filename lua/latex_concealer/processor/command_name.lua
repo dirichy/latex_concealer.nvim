8b9434b69f108ba2a61fcbef7d38f4f32e54868f
dirichy <byl315121270@gmail.com>
2025-10-07 21:08:44 +0800

    update

--- lua/latex_concealer/processor/command_name.lua
+++ lua/latex_concealer/processor/command_name.lua
@@ -1,6 +1,6 @@
 local highlight = require("latex_concealer.extmark").config.highlight
 local M = {}
-for key, value in pairs(require("nvimtex.symbol.items")) do
+for key, value in pairs(require("nvimtex.latex.items")) do
 	if type(value.conceal) == "string" then
 		M[key] = { value.conceal, highlight[value.class] }
 	end
